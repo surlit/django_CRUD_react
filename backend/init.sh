bce4bae1b6dc0fe51f3e50fb47a79b9e316ace0a
Luis_silva <surlit29@gmail.com>
2025-09-05 09:21:28 -0400

    [UPD] se valida que si existe el entorno virtual no lo vuelva a crear y si ya esta django instalado sal la instalacion del requieremetns

--- backend/init.sh
+++ backend/init.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-#Crear entorno virtual
-python3 -m venv env
 
-#levantar entorno virtual 
-source env/bin/activate
+# Nombre del entorno virtual
+ENV_DIR="env"
 
-#instalamos django
-pip install -r requirements.txt
+# Verificar si el entorno virtual ya existe
+if [ -d "$ENV_DIR" ]; then
+    echo "El entorno virtual ya existe. Activándolo..."
+else
+    # Crear entorno virtual
+    echo "Creando un nuevo entorno virtual..."
+    python3 -m venv "$ENV_DIR"
+fi
 
-#realizar migraciones
-python3 manage.py makemigrations
-python3 manage.py migrate
+# Levantar entorno virtual
+source "$ENV_DIR/bin/activate"
 
-#levantar django
-python3 manage.py runserver 8001
+# Instalar Django si no está instalado
+if ! python -c "import django" &> /dev/null; then
+    echo "Instalando Django..."
+    pip install -r requirements.txt
+else
+    echo "Django ya está instalado."
+fi
+
+# Realizar migraciones
+echo "Realizando migraciones..."
+python manage.py makemigrations
+python manage.py migrate
+
+# Levantar Django
+echo "Levantando el servidor de Django en el puerto 8001..."
+python manage.py runserver 8001
